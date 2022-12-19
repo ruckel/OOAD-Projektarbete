1c4bd095b4371bc43f41ff569d7e9b8b672bf7a7
Melinda <melinda_walter@hotmail.com>
2022-12-19 13:09:50 +0100

    homescreen with background fix

--- src/ui/HomeScreen.java
+++ src/ui/HomeScreen.java
@@ -22,22 +22,22 @@ public class HomeScreen implements Draw {
 
         g2.setFont(g2.getFont().deriveFont(Font.BOLD, 110F));
         String title = "Space War";
-        g2.drawString(title, gp.size/2, gp.size*2);
+        g2.drawString(title, gp.size/2, gp.size*3);
         getCenterForX(g2, title, gp);
 
         g2.setColor(Color.black);
-        g2.drawString(title, gp.size / 2 - 5, gp.size * 2 + 5);
+        g2.drawString(title, gp.size / 2 - 5, gp.size * 3+5);
 
         g2.setColor(Color.white);
-        g2.drawString(title, gp.size / 2, gp.size * 2);
+        g2.drawString(title, gp.size / 2, gp.size * 3);
 
 
         g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 20F));
-        g2.drawString("press enter to start new game", gp.size/2+155, gp.size*3);
+        g2.drawString("press enter to start new game", gp.size/2+155, gp.size*4);
         getCenterForX(g2, "press enter to start new game", gp);
 
         g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 20F));
-        g2.drawString("  or esc to exit screen", gp.size/2+175, gp.size*3+40);
+        g2.drawString("  or esc to exit screen", gp.size/2+175, gp.size*5-30);
         getCenterForX(g2, "  or esc to exit screen", gp);
 
     }
