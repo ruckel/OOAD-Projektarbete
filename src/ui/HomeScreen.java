135b855f1f7fe93400e073734d0f4ed380fbf6cc
Melinda <melinda_walter@hotmail.com>
2022-12-19 12:20:27 +0100

    homescreen with background

--- src/ui/HomeScreen.java
+++ src/ui/HomeScreen.java
@@ -5,17 +5,20 @@ import main.GamePanel;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
-
 public class HomeScreen implements Draw {
-    Image image;
+    private final BufferedImage homebackground;
+
+    public HomeScreen() {
+       homebackground = loadImage("homebackground");
+    }
     @Override
     public void draw(Graphics2D g2, GamePanel gp) {
 
-        g2.setBackground(Color.DARK_GRAY);
-        g2.fillRect(0, 0, gp.width, gp.height);
+
+        g2.drawImage(homebackground, 0, 0, gp.width, gp.height, null);
+        //g2.fillRect(0, 0, gp.width, gp.height);
 
         g2.setFont(g2.getFont().deriveFont(Font.BOLD, 110F));
         String title = "Space War";
@@ -34,17 +37,22 @@ public class HomeScreen implements Draw {
         getCenterForX(g2, "press enter to start new game", gp);
 
         g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 20F));
-        g2.drawString("or esc to exit screen", gp.size/2+175, gp.size*3+40);
-        getCenterForX(g2, "or esc to exit screen", gp);
+        g2.drawString("  or esc to exit screen", gp.size/2+175, gp.size*3+40);
+        getCenterForX(g2, "  or esc to exit screen", gp);
 
     }
     private int getCenterForX(Graphics2D g2, String text, GamePanel gp){
         int length = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
         return gp.width/2 - length/2;
+
     }
-
-    public void background() throws IOException {
-        image = ImageIO.read(new File("resources/homebackground.jpeg"));
-
+    private BufferedImage loadImage(String name){
+        BufferedImage img = null;
+        try{
+            img = ImageIO.read(getClass().getResourceAsStream("/" + name + ".png"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return img;
     }
 }
